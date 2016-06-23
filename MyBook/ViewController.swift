0193b933fce9e5480ea8fffe4bbda089acf5a364
F839-002 M <mf839-002@70-12-244-99.pools.spcsdns.net>
2016-06-23 09:51:51 +0900

    Initial Commit

---
+++ MyBook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyBook
+//
+//  Created by MF839-002 on 2016. 6. 23..
+//  Copyright © 2016년 MF839-002. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
